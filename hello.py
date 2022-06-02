04c044280f0c4b09b7986fca156fd46efa9b616a
Pavel <noviydah@gmail.com>
2022-06-02 08:58:45 +0300

    add python

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
-name = ("Pavel", "Vitaliy")
-print(name[0][1])
+s=set()
+s.add(1)
+s.add(2)
+s.add(3)
+print(s)
+s.remove(2)
+print(s)
+#print(f"We have tuple {len(s)} elemenis ")
+#family={"son":"Vitalik", "dad":"Pavel"}
+#print (family["son"])
+#for character in names:
+ #   print(character)
+
