3582b3455ff0bc4467f9f55105752f9ce0e10ad1
Pavel <noviydah@gmail.com>
2022-06-10 09:39:41 +0300

    add python

--- hello.py
+++ hello.py
@@ -1,13 +1,30 @@
-s=set()
-s.add(1)
-s.add(2)
-s.add(3)
-print(s)
-s.remove(2)
-print(s)
-#print(f"We have tuple {len(s)} elemenis ")
-#family={"son":"Vitalik", "dad":"Pavel"}
-#print (family["son"])
-#for character in names:
- #   print(character)
+
+
+class Flight():
+    def __init__(self,capacity):
+        self.capacity=capacity
+        self.passangers=[]
+    def add_passanger(self, name):
+        if not self.open_seats():
+            return False
+        self.passangers.append(name)
+        return True
+    def open_seats(self):
+        return self.capacity-len(self.passangers)
+
+flight = Flight(3)
+
+people = ["Vitaliy", "Ludmila", "Pavel", "Martin"]
+for person in people: 
+    success=flight.add_passanger(person)
+    if success :
+        print(f"Add {person} to flight successfully")
+    else:
+        print(f"No available seat fo {person}")
+
+input()
+
+
+
+
 
